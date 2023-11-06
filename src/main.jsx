34f0fef63d17d6a24c308e3457acc16a88f04df3
han0224 <dbn0224@gmail.com>
2023-11-06 16:52:47 +0900

    hashrouter 로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={"/portfolio"}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
+
+// github page 에서 새로고침시 404가 생김
+// hashrouter 하면 그런 문제가 없음
+// 검색엔진이 못읽는 단점이 있기는 한데, 프로젝트 상세내용은 검색엔진이 굳이 읽을 필요가 없음
